d5683fb87cb3b36a4fd610358603b28572f9bbd3
FerryFL01 <ferryfebrian.lim@gmail.com>
2026-05-12 14:03:20 +0700

    feat: metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,40 +5,42 @@ import { Toaster } from "@/components/ui/sonner";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://ferryfebrian.com'),
-  alternates: {
-    canonical: '/',
-  },
-  title: "Ferry Febrian | Portfolio",
-  icons: {
-    icon: "/logo_porto_ff.png"
-  }
+	metadataBase: new URL("https://ferryfebrian.com"),
+	alternates: {
+		canonical: "/",
+	},
+	title: "Ferry Febrian | Portfolio",
+	description:
+		"a Fresh Graduate in Computer Science from Bina Nusantara University and a Software Engineer specialized in Web Development.",
+	icons: {
+		icon: "/logo_porto_ff.png",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Toaster />
-      </body>
-      <GoogleAnalytics gaId="G-R9YD32C53Y" />
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+				<Toaster />
+			</body>
+			<GoogleAnalytics gaId="G-R9YD32C53Y" />
+		</html>
+	);
 }
