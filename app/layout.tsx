a010564fbd948df557755f2ddb3afc29f641c9cb
FerryFL01 <ferryfebrian.lim@gmail.com>
2026-09-17 14:10:13 +0700

    feat: update cv

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
+	display: "swap",
 });
 
 const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
 	subsets: ["latin"],
+	display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -38,7 +39,6 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				{children}
-				<Toaster />
 			</body>
 			<GoogleAnalytics gaId="G-R9YD32C53Y" />
 		</html>
